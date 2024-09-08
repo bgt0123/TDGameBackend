30299b6ac6718f24da8e7edf012ba17907b13919
Bennet Göttsche - I20 <bennet.goettsche@nordakademie.de>
2024-09-08 10:24:28 +0200

    Initial Commit

---
+++ src/test/kotlin/com/example/tdgamebackend/TdGameBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tdgamebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TdGameBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
